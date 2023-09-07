f1f6ecb3f019d887e396d66a74ebaee99a6095d8
Nathan Nkweto <nathannkweto87@gmail.com>
2023-09-07 14:44:06 +0100

    solution for task 5 and 6

---
+++ 0x00-hello_world/6-size.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+/**
+ * main - prints the sizes of the various types
+ * Return: 0
+ */
+int main(void)
+{
+	printf("Size of a char: %zu bytes\n", sizeof(char));
+	printf("Size of a float: %zu bytes\n", sizeof(float));
+	printf("Size of an int: %zu bytes\n", sizeof(int));
+	printf("Size of a long int: %zu bytes\n", sizeof(long int));
+	Return (0);
+}
+	
