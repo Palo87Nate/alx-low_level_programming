e267518ebf6c0f418a6be63af8090a870020fe9f
Nathan Nkweto <nathannkweto87@gmail.com>
2023-09-07 14:45:30 +0100

    major correction for task 6

--- 0x00-hello_world/6-size.c
+++ 0x00-hello_world/6-size.c
@@ -9,6 +9,6 @@ int main(void)
 	printf("Size of a float: %zu bytes\n", sizeof(float));
 	printf("Size of an int: %zu bytes\n", sizeof(int));
 	printf("Size of a long int: %zu bytes\n", sizeof(long int));
-	Return (0);
+	return (0);
 }
 	
