54d3ced73ed05e061d0c46a31a2b30cd3d898aa4
Nathan Nkweto <nathannkweto87@gmail.com>
2023-09-07 14:36:35 +0100

    solution for task 5

---
+++ 0x00-hello_world/5-printf.c
@@ -0,0 +1,10 @@
+#include <stdio.h>
+/**
+ * main - prints the specified text in the printf function
+ * Return: 0
+ */
+int main(void)
+{
+	printf("with proper grammar, but the outcome is a piece of art,");
+	return (0);
+}
