78cae44af11bbda77ef0f88237b37728647d5f8d
Nathan Nkweto <nathannkweto87@gmail.com>
2023-09-08 18:49:15 +0100

    Advanced tasks

--- 0x01-variables_if_else_while/101-print_comb4.c
+++ 0x01-variables_if_else_while/101-print_comb4.c
@@ -11,25 +11,23 @@ int main(void)
 	int d;
 	int e;
 
-	for (c = 0; c < 10; c++)
+	for (c = 0; c <= 7; c++)
 	{
-		for (d = 0; d < 10; d++)
+		for (d = c + 1; d <= 8; d++)
 		{
-			for (e = 0; e < 10; e++)
+			for (e = d + 1; e <= 9; e++)
 			{
 				putchar(c + '0');
 				putchar(d + '0');
 				putchar(e + '0');
 				if (c != 7 || d != 8 || e != 9)
-					{
+				{
 						putchar(',');
 						putchar(' ');
-					}
 				}
 			}
+		}
 	}
 	putchar('\n');
 	return (0);
 }
-						
-
