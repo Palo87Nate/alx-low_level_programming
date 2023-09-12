b1f36963a539cfa01ab84b83a8cfce0f58fdc5c6
Nathan Nkweto <nathannkwet87@gmail.com>
2023-09-12 16:15:56 -0700

    my solutions for optional tasks

--- 0x02-functions_nested_loops/104-fibonacci.c
+++ 0x02-functions_nested_loops/104-fibonacci.c
@@ -6,27 +6,19 @@
  */
 int main(void)
 {
-	int fib1 = 1, fib2 = 2;
-	int count = 2;
-	
-	printf("%d, %d, ", fib1, fib2);
-	while (count < 98) 
-	{
-		int next_fib = fib1 + fib2;
-	
-		printf("%d", next_fib);
-		count++;
-		if (count < 98) 
-		{
-			printf(", ");
-		}
-		else 
-		{
-            
-			printf("\n");
-		}
-		fib1 = fib2;
-		fib2 = next_fib;
-	}
-	return 0;
+    int i = 0;
+    unsigned long int a = 0, b = 1, next = 0;
+
+    while (i < 98)
+    {
+        next = a + b;
+        a = b;
+        b = next;
+        printf("%lu", next);
+        if (i < 97)
+            printf(", ");
+        i++;
+    }
+    putchar('\n');
+    return (0);
 }
