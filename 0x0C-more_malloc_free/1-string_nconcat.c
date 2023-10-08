b6862ac8eca4eced0f0a9ecf4cd7b2d570db8731
Nathan Nkweto <nathannkwet87@gmail.com>
2023-10-08 13:55:41 -0700

    second task of mandatory tasks

--- 0x0C-more_malloc_free/1-string_nconcat.c
+++ 0x0C-more_malloc_free/1-string_nconcat.c
@@ -2,40 +2,43 @@
 #include <stdlib.h>
 
 /**
- * string_nconcat - concatenates strings
- * @s1: first string to concatenate
- * @s2: second string to concatenate
- * @n: number of bytes to add from s2
- * Return: pointer
+ * string_nconcat - concatenates two strings.
+ * @s1: The string #1 to concatenate.
+ * @s2: The string #2 to concatenate.
+ * @n: bytes to add from s2
+ * Return: p
  */
+
 char *string_nconcat(char *s1, char *s2, unsigned int n)
 {
-	unsigned int a, b, x, y;
-	char *pointer;
+    if (s1 == NULL)
+        s1 = "";
+    if (s2 == NULL)
+        s2 = "";
 
-	if (s1 == 0)
-		s1 = "";
-	if (s2 == 0)
-		s2 = "";
-	a = 0;
-	while (s1[a] != '\0')
-		a++;
-	b = 0;
-	while (s2[b] != '\0')
-		b++;
-	if (n > a)
-		n = b;
-	pointer = malloc(sizeof(char) * (a + n + 1));
-	if (pointer == 0)
-		return (NULL);
-	for (x = 0; x < a; x++)
-		pointer[x] = s1[x];
-	b = 0;
-	for (y = x; y < (a + n); y++)
-	{
-		pointer[y] = s2[b];
-		b++;
-	}
-	p[y] = '\0';
-	return (pointer);
+    unsigned int len1 = 0;
+    while (s1[len1] != '\0')
+        len1++;
+
+    unsigned int len2 = 0;
+    while (s2[len2] != '\0' && len2 < n)
+        len2++;
+
+    char *p = (char *)malloc(sizeof(char) * (len1 + len2 + 1));
+
+    if (p == NULL)
+        return (NULL);
+
+    unsigned int i, j;
+
+    for (i = 0; i < len1; i++)
+        p[i] = s1[i];
+
+    for (j = 0; j < len2; j++)
+        p[i + j] = s2[j];
+
+    p[i + j] = '\0';
+
+    return (p);
 }
+
