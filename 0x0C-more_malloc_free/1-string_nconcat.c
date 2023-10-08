26bdc52cf51a82c2a5cd68235caa001f8c0f1c4d
Nathan Nkweto <nathannkwet87@gmail.com>
2023-10-08 14:16:25 -0700

    mandatory tasks

--- 0x0C-more_malloc_free/1-string_nconcat.c
+++ 0x0C-more_malloc_free/1-string_nconcat.c
@@ -1,44 +1,40 @@
 #include "main.h"
 #include <stdlib.h>
-
 /**
  * string_nconcat - concatenates two strings.
  * @s1: The string #1 to concatenate.
  * @s2: The string #2 to concatenate.
  * @n: bytes to add from s2
- * Return: p
+ * Return: pointer with the address of the array created .
  */
-
 char *string_nconcat(char *s1, char *s2, unsigned int n)
 {
-    if (s1 == NULL)
-        s1 = "";
-    if (s2 == NULL)
-        s2 = "";
+	unsigned int i, j, x, y;
+	char *p;
 
-    unsigned int len1 = 0;
-    while (s1[len1] != '\0')
-        len1++;
-
-    unsigned int len2 = 0;
-    while (s2[len2] != '\0' && len2 < n)
-        len2++;
-
-    char *p = (char *)malloc(sizeof(char) * (len1 + len2 + 1));
-
-    if (p == NULL)
-        return (NULL);
-
-    unsigned int i, j;
-
-    for (i = 0; i < len1; i++)
-        p[i] = s1[i];
-
-    for (j = 0; j < len2; j++)
-        p[i + j] = s2[j];
-
-    p[i + j] = '\0';
-
-    return (p);
+	if (s1 == NULL)
+		s1 = "";
+	if (s2 == NULL)
+		s2 = "";
+	i = 0;
+	while (s1[i] != '\0')
+		i++;
+	j = 0;
+	while (s2[j] != '\0')
+		j++;
+	if (n > j)
+		n = j;
+	p = malloc(sizeof(char) * (i + n + 1));
+	if (p == NULL)
+		return (NULL);
+	for (x = 0; x < i; x++)
+		p[x] = s1[x];
+	j = 0;
+	for (y = x; y < (i + n); y++)
+	{
+		p[y] = s2[j];
+		j++;
+	}
+	p[y] = '\0';
+	return (p);
 }
-
