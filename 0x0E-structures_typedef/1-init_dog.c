af31171352624638b1acbb0f4d16341849e75df4
Nathan Nkweto <nathannkwet87@gmail.com>
2023-10-10 08:10:54 -0700

    First three tasks

---
+++ 0x0E-structures_typedef/1-init_dog.c
@@ -0,0 +1,17 @@
+#include "dog.h"
+/**
+ * init_dog - initialize variable  struct dog
+ * @d: struct dog
+ * @name:  name
+ * @age: dog
+ * @owner: owner of the dog
+ * Description:some information of a dog
+ */
+void init_dog(struct dog *d, char *name, float age, char *owner)
+{
+	if (d == 0)
+		return;
+	d->name = name;
+	d->age = age;
+	d->owner = owner;
+}
---
+++ 0x0E-structures_typedef/dog.h
@@ -0,0 +1,15 @@
+#ifndef DOG
+#define DOG
+/**
+ * struct dog - some details of a dog
+ * @name: name
+ * @age: age
+ * @owner: owner of dog
+ */
+struct dog
+{
+	char *name;
+	float age;
+	char *owner;
+};
+#endif
