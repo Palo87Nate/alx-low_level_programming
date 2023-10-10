5fd479007673179f497c092ba8fbec44e7af7397
Nathan Nkweto <nathannkwet87@gmail.com>
2023-10-10 07:03:34 -0700

    all work done here

--- 0x0D-preprocessor/1-pi.h
+++ 0x0D-preprocessor/1-pi.h
@@ -1,1 +1,4 @@
+#ifndef pi
+#define pi
 #define PI 3.14159265359
+#endif
--- 0x0D-preprocessor/3-function_like_macro.h
+++ 0x0D-preprocessor/3-function_like_macro.h
@@ -1,1 +1,4 @@
-#define ABS(x) (((x) > (0)) ? (x) : ((x) * (-1))
+#ifndef function_like_macro
+#define function_like_macro
+#define ABS(x) (((x) < (0)) ? ((x) * (-1)) : (x))
+#endif
