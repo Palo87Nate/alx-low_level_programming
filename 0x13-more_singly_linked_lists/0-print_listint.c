abcdb3a95a19621774ecc9ab595166c707036295
Nathan Nkweto <nathannkweto87@gmail.com>
2023-10-23 23:11:40 +0100

    first 4 tasks

---
+++ 0x13-more_singly_linked_lists/0-print_listint.c
@@ -0,0 +1,26 @@
+#include "lists.h"
+
+/**
+* print_listint - prints all elements of a list
+* @h: head of the linked list
+* Return: number of nodes
+*/
+
+size_t print_listint(const listint_t *h)
+{
+	size_t count = 0;
+	const listint_t *tmp;
+
+	tmp = h;
+	if (h == '\0')
+	{
+		return (0);
+	}
+	while (tmp)
+	{
+		printf("%d\n", tmp->n);
+		count++;
+		tmp = tmp->next;
+	}
+	return (count);
+}
